312e88aeed311eed5f4e2b8a2f5ef38dd8936308
Jasper Jee <jasper@kiratech.com.au>
2024-01-11 21:15:49 +0800

    feat: add navigation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,19 +8,23 @@
     :loop="true"
     :effect="'fade'"
     :autoplay="{
-      delay: 2000,
+      delay: 6000,
+      disableOnInteraction: false,
     }"
     class="select-none"
     @swiper="onSwiper"
   >
     <SwiperSlide v-for="data in swiperData" style="height: 100vh">
-      <nuxt-link :to="localePath(data.link)">
-        <nuxt-img
-          preset="cover"
-          class="h-full w-full object-cover"
-          :src="data.image"
-        />
-      </nuxt-link>
+      <div class="absolute inset-y-0 inset-x-0 z-10 flex pagination">
+        <div class="left w-1/3 h-full" @click="swiper.slidePrev()"></div>
+        <nuxt-link :to="localePath(data.link)" class="center w-1/3"></nuxt-link>
+        <div class="right w-1/3 h-full" @click="swiper.slideNext()"></div>
+      </div>
+      <nuxt-img
+        preset="cover"
+        class="h-full w-full object-cover"
+        :src="data.image"
+      />
     </SwiperSlide>
   </Swiper>
 </template>
@@ -35,43 +39,55 @@ const onSwiper = (newSwiper) => {
 const swiperData = ref([
   {
     image: "/images/mazco-1.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-2.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-3.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-4.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-5.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-6.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-7.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-8.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-9.jpg",
-    link: "/",
+    link: "/news",
   },
   {
     image: "/images/mazco-10.jpg",
-    link: "/",
+    link: "/news",
   },
 ]);
 </script>
+
+<style scoped>
+.pagination .left {
+  cursor: url("/assets/img/prev.png") 30 30, auto;
+}
+.pagination .right {
+  cursor: url("/assets/img/next.png") 30 30, auto;
+}
+.pagination .center {
+  cursor: url("/assets/img/open.png") 30 30, auto;
+}
+</style>
